fe667bf11214ca9c4fc2efeee6d3d9bf79e34a63
Brandon <162996462+garard@users.noreply.github.com>
2024-03-16 23:37:24 +1000

    Add files via upload
    used plt to show results instead of printing in terminal
    uses noto jp font to display text properly
    squashed bug preventing replay without exiting script

--- app.py
+++ app.py
@@ -3,26 +3,24 @@ import os
 import getch
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # External vocab list
 df = pd.read_csv("vocab.csv")
-
-
-#Prepare array for answer storage in dictionary
-answerHistory = np.empty((99, 5), dtype=np.dtype('U100'))
-
 
 # Clear the terminal screen
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
-# All variables contained here
+# Variables
 gameInfo = {
     'answer': '',
     'answerColumn': 0,
     'answerRow': 0,
-    'history': answerHistory,
+    'history': np.empty((99, 5), dtype=np.dtype('U100')),
     'correct': 0,
     'language': '',
     'notLanguage': '',
@@ -36,20 +34,41 @@ gameInfo = {
     'word': '',
 }
 
+# Reset Variables
+def gameReset(gameInfo):
+    numQuestions = gameInfo['questions']
+    gameInfo.clear()
+    gameInfo.update({
+        'answer': '',
+        'answerColumn': 0,
+        'answerRow': 0,
+        'history': np.empty((99, 5), dtype=np.dtype('U100')),
+        'correct': 0,
+        'language': '',
+        'notLanguage': '',
+        'option': 0,
+        'optionMenu': 0,
+        'playerAnswer': '',
+        'question': 1,
+        'questionColumn': 0,
+        'questionRow': 0,
+        'questions': numQuestions,
+        'word': '',
+    }
+)
 
 # Home Menu
 def menu(gameInfo):
+    clear_terminal()
     while not (1 <= gameInfo['option'] <= 3):
-        print("Current directory:", os.getcwd())
         print("Select an option:\n1: Start\n2: Options\n3: Exit\n")
         try:
             gameInfo['option'] = int(getch.getch())
             return gameInfo['option']
         except ValueError:
             gameInfo['option'] = 0
-
-
-# Randoms question from external table
+        
+# Randoms question from external csv
 def newQuestion(gameInfo):
     gameInfo['answerColumn'] = random.choice(df.columns)
     if gameInfo['answerColumn'] == 'Japanese':
@@ -62,19 +81,17 @@ def newQuestion(gameInfo):
     gameInfo['answer'] = df.loc[gameInfo['answerRow'] , gameInfo['answerColumn']]
     gameInfo['word'] = df.loc[gameInfo['answerRow'] , gameInfo['notLanguage']]
 
-
 # General Question Loop
 def gameQuestion(gameInfo):
     #print(f"The answer is {gameInfo['answer']}")
     print(f"Correct: {gameInfo['correct']}")
     gameInfo['playerAnswer'] = input(f"Question {gameInfo['question']} of {gameInfo['questions']}:\n\nWhat is {gameInfo['word']} in {gameInfo['language']}?\n\n\t")
 
-
 # Checks input against expected answer, tallys correct answers
 def checkAnswer(gameInfo):
     gameInfo['history'][gameInfo['question'] - 1][3] = gameInfo['word']
     gameInfo['history'][gameInfo['question'] - 1][4] = gameInfo['language']
-    if gameInfo['playerAnswer'] == gameInfo['answer']:
+    if gameInfo['answer'].lower() in gameInfo['playerAnswer'].lower():
         gameInfo['correct'] += 1
         print("Correct!\n")
         gameInfo['history'][gameInfo['question'] - 1][2] = "O"  # Adjust index to start from 0
@@ -86,10 +103,10 @@ def checkAnswer(gameInfo):
     gameInfo['history'][gameInfo['question'] - 1][1] = gameInfo['answer']  # Adjust index to start from 0
     gameInfo['history'][gameInfo['question'] - 1][0] = gameInfo['playerAnswer']  # Adjust index to start from 0
 
-
 # Main script flow
 def game(gameInfo):
     while gameInfo['option'] == 1:
+        gameInfo['question'] = 1
         for gameInfo['question'] in range(1, gameInfo['questions'] + 1):
             newQuestion(gameInfo)
             gameQuestion(gameInfo)
@@ -98,59 +115,43 @@ def game(gameInfo):
         results(gameInfo)
         gameInfo['option'] = 0
 
-
 # Draws Results table then sends users back to home menu
 def results(gameInfo):
     clear_terminal()
-    gameInfo['question'] = 0
-    print(f"\n\tYou answered {gameInfo['correct']} questions correctly!\n")
-    print("Q\t|\tWord\t\t|\tYour Answer\t|\tCorrect Answer\t|\tCorrect")
-    print("_________________________________________________________________________________________________________")
+    data = []
+
+    font_path = 'font.ttf'  # Your font path goes here
+    fm.fontManager.addfont(font_path)
+    prop = fm.FontProperties(fname=font_path)
+
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.sans-serif'] = prop.get_name()
+
+    print(f"\n\n\tYou answered {gameInfo['correct']} questions correctly!\n")
+
     for gameInfo['question'] in range(0, gameInfo['questions']):
-        print(f"{gameInfo['question']+1}", end='\t|\t')
+        row_data = []
+        row_data.append(str(gameInfo['question'] + 1))# Question Number
+        row_data.append(f"{gameInfo['history'][gameInfo['question']][3]}") # Word
+        row_data.append(f"{gameInfo['history'][gameInfo['question']][0]}") # Your Answer
+        row_data.append(f"{gameInfo['history'][gameInfo['question']][1]}") # Correct Answer
+        row_data.append(f"{gameInfo['history'][gameInfo['question']][2]}") # O or X
+        data.append(row_data)
 
-    # Word
-        if gameInfo['history'][gameInfo['question']][4] == 'Japanese':
-            if len(gameInfo['history'][gameInfo['question']][3]) < 8:
-                print(f"{gameInfo['history'][gameInfo['question']][3]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][3]}", end='\t|\t')
-        
-        if gameInfo['history'][gameInfo['question']][4] == 'English':
-            if len(gameInfo['history'][gameInfo['question']][3]) < 5:
-                print(f"{gameInfo['history'][gameInfo['question']][3]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][3]}", end='\t|\t')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.axis('tight')
+    ax.axis('off')
 
-    # Your Answer    
-        if gameInfo['history'][gameInfo['question']][4] == 'Japanese':
-            if len(gameInfo['history'][gameInfo['question']][0]) < 8:
-                print(f"{gameInfo['history'][gameInfo['question']][0]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][0]}", end='\t|\t')
-        
-        if gameInfo['history'][gameInfo['question']][4] == 'English':
-            if len(gameInfo['history'][gameInfo['question']][0]) <= 5:
-                print(f"{gameInfo['history'][gameInfo['question']][0]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][0]}", end='\t|\t')
+    table = ax.table(cellText=data, colLabels=['Q', 'Word', 'Your Answer', 'Correct Answer', 'Correct'],
+                    loc='center', cellLoc='center', colColours=['lightblue']*5)
+    table.set_fontsize(14)
+    table.scale(1, 2)
 
-    # Correct Answer    
-        if gameInfo['history'][gameInfo['question']][4] == 'Japanese':
-            if len(gameInfo['history'][gameInfo['question']][1]) <= 5:
-                print(f"{gameInfo['history'][gameInfo['question']][1]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][1]}", end='\t|\t')
-        
-        if gameInfo['history'][gameInfo['question']][4] == 'English':
-            if len(gameInfo['history'][gameInfo['question']][3]) <= 5:
-                print(f"{gameInfo['history'][gameInfo['question']][1]}", end='\t\t|\t')
-            else:
-                print(f"{gameInfo['history'][gameInfo['question']][1]}", end='\t|\t')
-        print(f"{gameInfo['history'][gameInfo['question']][2]}")
-    print("\nPress any key to go back to home menu")
+    plt.show()
+
+    print("\tPress any key to go back to home menu")
     getch.getch()
-
+    gameReset(gameInfo)
 
 # Settings Menu
 def settings(gameInfo):
@@ -182,20 +183,17 @@ def settings(gameInfo):
             gameInfo['option'] = 0
             return gameInfo['option']
 
-
 # Menu Navigation
 while not (gameInfo['option'] == 3):
     if gameInfo['option'] == 0:
         clear_terminal()
-        gameInfo['option'] = menu(gameInfo)
+        menu(gameInfo)
     elif gameInfo['option'] == 1:
         clear_terminal()
         print('がんばって!')
-        gameInfo['option'] = game(gameInfo)
+        game(gameInfo)
     elif gameInfo['option'] == 2:
         clear_terminal()
-        gameInfo['option'] = settings(gameInfo)
-    elif gameInfo['option'] == 3:
-        exit
-    elif gameInfo['option'] == 4:
-        pass
+        settings(gameInfo)
+exit
+
